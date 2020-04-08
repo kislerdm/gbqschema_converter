a635aee393c3d20f22048b804dbe050cf4a64ad9
Dmitry Kisler <admin@dkisler.de>
2020-04-09 00:49:38 +0200

    fixes order

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ python3.7 ${PWD}/setup.py sdist bdist_wheel
 
 echo "Validate the build"
 if [ $(python3.7 -m twine check ${PWD}/dist/* | grep FAIL | wc -l) -gt 0 ]; then
-    exit
     echo "twine validation failed"
+    exit
 fi
 echo "Valide!"
