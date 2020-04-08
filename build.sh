7f3f04898d03a0359575e383ed301fadf6b8a731
Dmitry Kisler <admin@dkisler.de>
2020-04-09 00:48:36 +0200

    adds build twine script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Build wheel"
+python3.7 ${PWD}/setup.py sdist bdist_wheel
+
+echo "Validate the build"
+if [ $(python3.7 -m twine check ${PWD}/dist/* | grep FAIL | wc -l) -gt 0 ]; then
+    exit
+    echo "twine validation failed"
+fi
+echo "Valide!"
